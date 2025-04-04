53e63bc34b5bb7f309b4b787a42e75a4c583f407
Goobis <daniellebarber1@yahoo.com>
2025-04-04 13:32:11 -0400

    I had to redo the app creation but now I have the framework working. Very base framework

--- app/modules/main/route.py
+++ app/modules/main/route.py
@@ -6,23 +6,5 @@ main_bp = Blueprint('main', __name__)
 main_controller = MainController()
 @main_bp.route('/', methods=['GET'])
 def index():
-    """ Example endpoint with simple greeting.
-    ---
-    tags:
-      - Example API
-    responses:
-      200:
-        description: A simple greeting
-        schema:
-          type: object
-          properties:
-            data:
-              type: object
-              properties:
-                message:
-                  type: string
-                  example: "Hello World!"
-    """
-    result=main_controller.index()
-    return make_response(jsonify(data=result))
+    return "<h1>Welcome to the Wildlife Sightings Visualizer!</h1>"
       
