61940ba3c75977d0ea3fd82a438cbab6437b5ac2
Goobis <daniellebarber1@yahoo.com>
2025-04-06 01:08:17 -0400

    I realized the temporary folium_map.html kept getting updated in github everytime, so I made a quick function to auto-delete it when closing the app (It's only supposed to be there as long as the website is up)

--- run.py
+++ run.py
@@ -1,4 +1,19 @@
 from app import create_app
+import os
+import atexit
 
 app = create_app()
-app.run(debug=True)
+MAP_FILE = "app/static/folium_map.html"
+
+#cleanup function to clear the folium map when exiting the app
+# this is to prevent sending a new folium_map to github each time
+def remove_folium_map():
+    if os.path.exists(MAP_FILE):
+        os.remove(MAP_FILE)
+        print(f"[Cleanup] Deleted: {MAP_FILE}")
+
+atexit.register(remove_folium_map)
+
+# Run app
+if __name__ == "__main__":
+    app.run(debug=True)
